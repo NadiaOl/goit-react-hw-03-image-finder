5c68140fb5b36f588c463c103cd2625e33f06da0
NadiaOl <115547708+NadiaOl@users.noreply.github.com>
2023-05-11 21:01:48 +0300

    подготовка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import React, {Component} from "react";
+import axios from 'axios';
+// import {Button} from './Button/Button';
+// import {ImageGallery} from './ImageGallery/ImageGallery';
+// import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+// import {Loader} from './Loader/Loader';
+// import {Modal} from './Modal/Modal';
+// import {Searchbar} from './Searchbar/Searchbar';
+
+import css from '../components/Searchbar/Searchbar.module.css';
+
+// переменные
+const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '34756592-add6791e980caa28afb1f7410';
+
+
+
+//  получаем ответ от бекэнда
+// async function fetchPhoto(seekedPhoto, page) {
+//     const arraySearchPhoto = await axios.get(`${BASE_URL}/?key=${KEY}&q=${seekedPhoto}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`)
+//     return arraySearchPhoto
+// }
+
+// export { fetchPhoto };
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+<header class="searchbar">
+  <form class="form">
+    <button type="submit" class="button">
+      <span class="button-label">Search</span>
+    </button>
+
+    <input
+      class="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
+<div class="overlay">
+  <div class="modal">
+    <img src="" alt="" />
+  </div>
+</div>
     </div>
   );
 };
